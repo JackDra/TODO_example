0b34cb6ff85cb1c91a0ae91d57fa226960003b8c
SarahDra98 <mugiwarasarah98@gmail.com>
2020-07-17 09:38:07 -0500

    check list editor

--- todo.py
+++ todo.py
@@ -1,10 +1,17 @@
 from traits.api import HasTraits, Str, List
+from traitsui.api import View, Item, CheckListEditor
 
 my_to_list = ['clean car', 'create youtube video', 'feed animals']
 
 class ToDo(HasTraits):
 
     todo_list = List(Str())
+    completed = List(Str())
+
+    my_view = View(
+        Item('completed', style='custom', editor=CheckListEditor(name='todo_list')),
+        Item('completed', style='readonly')
+    )
 
 my_todo = ToDo(todo_list=my_to_list)
 
